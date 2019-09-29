a2a77d0cbac839d4bfb934f0bae15ad0f1f024dd
Riotpiaole <Rock.Liang@titus.com>
2019-09-29 06:21:36 -0400

    Add data.json for statistics. - Add resolution

--- video_compression.py
+++ video_compression.py
@@ -3,7 +3,7 @@ import numpy as np
 import requests , json , time
 import argparse
 import os
-
+from tqdm import tqdm
 # login
 log_req = requests.post(
     "https://api.wrnch.ai/v1/login",
@@ -23,17 +23,18 @@ def get_result(jobid , access_token = access_token):
 
 def request_wrnchAI(img_path = '10.png',key="annotated_media"):
     # header = "Bearer {}".format(access_token)
-    files = {'media': open( img_path,'rb')}
-    test_annotated = requests.post(
-        "https://api.wrnch.ai/v1/jobs",
-        data={
-            "work_type": key,
-            "heads": True,
-            "tracking": True},
-        files=files,
-        headers={"Authorization": "Bearer {}".format(access_token)}
-        )
-    print(test_annotated)
+    with open( img_path,'rb') as fp:
+        files = {'media':  fp }
+        test_annotated = requests.post(
+            "https://api.wrnch.ai/v1/jobs",
+            data={
+                "work_type": key,
+                "heads": True,
+                "tracking": True},
+            files=files,
+            headers={"Authorization": "Bearer {}".format(access_token)}
+            )
+        print(test_annotated)
     return test_annotated
 
 def show_img(img):
@@ -74,16 +75,20 @@ def run_video(video):
 class VideoProcessor:
 
     def __init__(self, input_video, output_video=None,\
-                                                 width=0, height=0, count=False):
+                    width=0, height=0, count=False, \
+                     json_job_id=None , annotated_job_id=None):
         self.input_video = input_video
         self.width = width
         self.height = height
         self.count = count
+        self.json_content  = None
         if not output_video:
             name, ext = os.path.splitext(input_video)
             self.output_video = '{i}_compressed{e}'.format(i=name, e=ext)
         else:
             self.output_video = output_video
+
+        if json_job_id: self.json_content = get_result(json_job_id).json()
 
     def read_video(self):
         images = []
@@ -115,57 +120,71 @@ class VideoProcessor:
 
     def write_video(self, frames):
         imgs = self.read_video()
-        out = cv2.VideoWriter(self.output_video,
-            cv2.VideoWriter_fourcc(*'DIVX'), 15, (self.width, self.height))
+        json_logs = {'nums_people_in_frame': []}
+        # out = cv2.VideoWriter(self.output_video,
+        #     cv2.VideoWriter_fourcc(*'DIVX'), 30, (self.width, self.height))
 
-        max_interval = self.get_time(args.input, 10)
+        max_interval = self.get_time(self.input_video , 10)
+        json_logs['frame'] = 10
+        json_logs['time_unit'] = 'min'
+
         print(max_interval)
 
-        for (img, frame) in zip(imgs , frames):
+        for frame_indx, (img, frame) in tqdm(enumerate(zip(imgs , frames)), total=len(frames)):
             persons = frame.get('persons', [])
-            if(len(persons) > 0):
-                out.write(img)
+            # if(len(persons) > 0):
+            #     out.write(img)
+            if (frame_indx + 1) % max_interval  == 0:
+                json_logs['nums_people_in_frame'].append(
+                    len(frame['persons']))
         print('There are {} people in the video'.\
                                             format(self.count_people(frames)))
-        out.release()
+        with open('data.json', 'w') as outfile:
+            json.dump(json_logs, outfile)
+        # out.release()
 
     def get_time(self, path_video, interval):
-        req = run_video(video)
 
-        tot_frames = len(json.loads(req.content)['frames'])
+        tot_frames = len(self.json_content.json()['frames'])
         video = cv2.VideoCapture(path_video)
         fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
 
-        ret_interval = (tot_frames//fps)*(60*interval)
+        ret_interval = (tot_frames//fps)//(60*interval)
         # release vid
         video.release()
         return ret_interval # return the number of frames
-        
-        
+
+    def run(self):
+        if not (self.json_content):
+            self.json_content = run_video(self.input_video)
+
+        # self.write_video(self.json_content)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Python program to compress ' 
-                                                        'surveillance videos.')
-    parser.add_argument('--input',
-                        help='path to input video file',
-                        )
-    parser.add_argument('--output',
-                        help='path to output video file',
-                        )
-    parser.add_argument('--count',
-                        help='Count how many people are in the video'
-                            ' and print the result',
-                        action='store_true')
-    parser.add_argument('--resolution',
-                        help='resolution in output')
-    args = parser.parse_args()
-    if args.resolution:
-        w, h = args.resolution.split('x')
-    else:
-        w, h = None, None
+    # parser = argparse.ArgumentParser(description='Python program to compress '
+    #                                                     'surveillance videos.')
+    # parser.add_argument('--input',
+    #                     help='path to input video file',
+    #                     )
+    # parser.add_argument('--output',
+    #                     help='path to output video file',
+    #                     )
+    # parser.add_argument('--count',
+    #                     help='Count how many people are in the video'
+    #                         ' and print the result',
+    #                     action='store_true')
+    # parser.add_argument('--resolution',
+    #                     help='resolution in output')
+    # args = parser.parse_args()
+    # if args.resolution:
+    #     w, h = args.resolution.split('x')
+    # else:
+    #     w, h = None, None
 
-    vp = VideoProcessor(args.input, args.output, w, h, args.count)
+    # vp = VideoProcessor(args.input, args.output, w, h, args.count)
+    vp = VideoProcessor("y2mate.com - mall_3_HB2acU5m5zU_360p.mp4","mall.mp4", json_job_id="bfa4dc19-d5a3-4e7e-8659-30f94a66d49e")
 
-    annotation = run_video(args.input)
-    vp.write_video(annoation.json()['frames'])
+    # annotation = run_video(args.input)
+    print("Hello World")
+    # vp.write_video(annoation.json()['frames'])
 
